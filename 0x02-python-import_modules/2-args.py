482fd2dacd747e987e41be63d4a029c1d04e5c4c
Annamuturi <muturiwannah83@gmail.com>
2023-05-11 09:32:18 -0700

    update

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
+
     import sys
+
     count = len(sys.argv) - 1
     print("{} ".format(count), end="")
+
     if count == 0:
-        print("argument.")
+        print("argument")
+
     else:
         if count == 1:
-            print("argument:".format(count))
+            print("ardument:".format(count))
         else:
             print("arguments:".format(count))
-        for i in range(1, len(sys.argv)):
-            print("{}: {}".format(i, sys.argv[i]))
+
+            for i in range(1, len(sys.argv)):
+                print("{}: {}".format(i, sys.argv[i]))
