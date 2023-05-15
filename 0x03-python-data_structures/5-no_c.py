1191303bf4658144258b03b8069fa659955a7c10
Annamuturi <muturiwannah83@gmail.com>
2023-05-15 02:04:11 -0700

    update

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
+
 def no_c(my_string):
-    final_str = ''
-    for char in my_string:
-        if char != 'c' and char != 'C':
-            final_str += char
-        return (final_str)
+    new_str = ""
+    for i in range(len(my_string)):
+        if my_string[i] != 'c' and my_string[i] != 'C':
+            new_str += my_string[i]
+    return (new_str)
