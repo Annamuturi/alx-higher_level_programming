9260acd22eca7a9b1c4a67550c9b6195948f4993
Annamuturi <muturiwannah83@gmail.com>
2023-05-15 01:56:47 -0700

    add : and remove white spaces

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
-def no_c(my_string)
+def no_c(my_string):
 
 
-final_str = ''
-for char in my_string:
-    if char != 'c' and char != 'C':
-        final_str += char
+    final_str = ''
+    for char in my_string:
+        if my_string != 'c' and my_string != 'C':
+            final_str += my_string
         return (final_str)
