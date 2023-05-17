3118f988079c2e1cb0b5b9d82b0f681781c6d19e
Annamuturi <muturiwannah83@gmail.com>
2023-05-17 09:56:54 -0700

    scores

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def best_score(a_dictionary):
+    first = 0
+    second = None
+    if type(a_dictionary) is dict:
+        for (key, value) in a_dictionary.items():
+            if value > first:
+                first = value
+                second = key
+    return (second)
